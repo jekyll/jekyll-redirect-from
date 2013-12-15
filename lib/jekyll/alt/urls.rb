862fd36f84bca2becfbc5560aa0a8ca2860a5866
Parker Moore <parkrmoore@gmail.com>
2013-12-15 12:39:59 -0500

    Initial import.

---
+++ lib/jekyll/alt/urls.rb
@@ -0,0 +1,9 @@
+require "jekyll/alt/urls/version"
+
+module Jekyll
+  module Alt
+    module Urls
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/alt/urls/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Alt
+    module Urls
+      VERSION = "0.0.1"
+    end
+  end
+end
