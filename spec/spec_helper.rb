8447b1ed892d731223b414e43f0abcfe5b78c17f
Parker Moore <parkrmoore@gmail.com>
2014-07-14 22:51:00 -0400

    Use be_truthy instead of be_true.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require "jekyll"
 require File.expand_path("lib/jekyll-redirect-from.rb")
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
